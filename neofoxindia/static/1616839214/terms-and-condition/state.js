7f5309e30316f6a022ae8adfe20dbbac638ba33a
ashvinijangid <ashvinijangid@gmail.com>
2021-03-27 10:00:27 +0000

    Update from https://github.com/its-ash/neofox/commit/3f8792fbaa5fa1b49c84b8b3a0463fa1c0a47da5

---
+++ neofoxindia/static/1616839214/terms-and-condition/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fneofoxindia\u002Fstatic\u002F1616839214",layout:"default",error:a,serverRendered:true,routePath:"\u002Fterms-and-condition",config:{app:{basePath:"\u002F",assetsPath:"\u002Fneofoxindia\u002F",cdnURL:a}}}}(null));
