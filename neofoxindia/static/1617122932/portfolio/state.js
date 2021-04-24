9ddfbc67c8a42a8b4908628a7fc2f9cc2ac24d3b
ashvinijangid <ashvinijangid@gmail.com>
2021-04-24 07:22:14 +0000

    Update from https://github.com/its-ash/neofox/commit/b70535e1eace73ec4dfe1400910a15ff8712a269

--- neofoxindia/static/1617122932/portfolio/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fneofoxindia\u002Fstatic\u002F1617122932",layout:"default",error:a,serverRendered:true,routePath:"\u002Fportfolio",config:{app:{basePath:"\u002F",assetsPath:"\u002Fneofoxindia\u002F",cdnURL:a}}}}(null));
